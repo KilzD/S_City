540e95bb591a9675239193668535b3ec4cef2b43
Vlad Drozdov <vyudrozdov@edu.hse.ru>
2019-06-02 17:09:12 +0300

    Try

--- info_page_script.js
+++ info_page_script.js
@@ -1,14 +1,120 @@
 function clicked(args){
     $(".active").removeClass("active");
     $(args).addClass("active");
+
+    var obj = document.getElementById("graph_holder");
+
     switch (args.innerText) {
         case ("День"):
+            document.getElementById("legend").style.visibility = "hidden";
+            obj.innerHTML = '<div id="chartContainer" style="width: 70%; height: 100%; position: static"></div>';
+
+            dataPoints1 = [];
+            dataPoints2=[];
+
+            chart = new CanvasJS.Chart("chartContainer", {
+                width: 900,
+                height: 183,
+                zoomEnabled: true,
+                axisY:{
+                    includeZero: true
+                },
+                toolTip: {
+                    shared: true
+                },
+                legend: {
+                    cursor:"pointer",
+                    verticalAlign: "top",
+                    fontSize: 22,
+                    fontColor: "dimGrey",
+                    itemclick : toggleDataSeries
+                },
+                data: [{
+                    type: "line",
+                    xValueType: "dateTime",
+                    yValueFormatString: "####.00",
+                    xValueFormatString: "hh:mm:ss TT",
+                    showInLegend: true,
+                    name: "Температура за бортом",
+                    dataPoints: dataPoints1
+                },
+                    {
+                        type: "line",
+                        xValueType: "dateTime",
+                        yValueFormatString: "####.00",
+                        showInLegend: true,
+                        name: "Температура входящей воды",
+                        dataPoints: dataPoints2
+                    }]
+            });
+
+            updateChart(20);
+            setInterval(function(){updateChart()}, updateInterval);
             break;
         case("Неделя"):
+            obj.innerHTML = '<img src="images/week.png" style="width: 70%; height: 100%">';
+            document.getElementById("legend").style.visibility = "visible";
             break;
         case("Месяц"):
-            break;
-        case("Год"):
+            obj.innerHTML = '<img src="images/month.png" style="width: 70%; height: 100%">'
+            document.getElementById("legend").style.visibility = "visible";
             break;
     }
+}
+
+var chart;
+
+function toggleDataSeries(e) {
+    if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+        e.dataSeries.visible = false;
+    }
+    else {
+        e.dataSeries.visible = true;
+    }
+    chart.render();
+}
+
+var dataPoints1 = [];
+var dataPoints2 = [];
+
+var updateInterval = 500;
+// initial value
+var yValue1 = 10;
+var yValue2 = 70;
+// generates first set of dataPoints
+
+var time = new Date;
+// starting at 9.30 am
+time.setHours(9);
+time.setMinutes(30);
+time.setSeconds(30);
+time.setMilliseconds(30);
+
+function updateChart(count) {
+    count = count || 1;
+    for (var i = 0; i < count; i++) {
+        time.setTime(time.getTime() + updateInterval);
+        $.ajax({
+                url: "http://10.20.3.26:8000/vlad",
+                type: "get",
+                async: false,
+                dataType: 'json',
+                success(data) {
+                    dataPoints1.push({
+                        x: time.getTime(),
+                        y: data.t_outside
+                    });
+                    dataPoints2.push({
+                        x: time.getTime(),
+                        y: data.t
+                    });
+                },
+                error(msg) {
+                    console.log(msg);
+                    console.log(this.url);
+                }
+            }
+        )
+    }
+    chart.render();
 }
--- map_script.js
+++ map_script.js
@@ -3,10 +3,197 @@ mapboxgl.accessToken = 'pk.eyJ1Ijoia2lsemQiLCJhIjoiY2p3Y3BrcGdlMHY4ZjQ5b2p4YWp1b
 var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v10',
-    center: [37.623889714544504, 55.741055286358545],
+    center: [37.525434494018555, 55.57664649599778],
     zoom: 15
+});
+
+var linking = "http://10.20.3.26:8000/";
+
+function find(){
+    map.setCenter({
+        lat: 55.577611459040156,
+        lng: 37.518823709011656
+    });
+}
+
+map.on('click', function () {
+
+    document.getElementById("info_hot_bowl").style.visibility = "hidden";
+    document.getElementById("info_house").style.visibility = "hidden";
+    document.getElementById("hot_bowl_temp").innerText = "Текущая температура: ";
+    document.getElementById("address_b").innerText = "Адрес котельной";
+    document.getElementById("house_temp").innerText = "Текущая температура: ";
+    document.getElementById("address_h").innerText = "Адрес дома";
+});
+
+map.on('click', 'client', function (e) {
+    document.getElementById("info_hot_bowl").style.visibility = "visible";
+    document.getElementById("info_house").style.visibility = "visible";
+    document.getElementById("hot_bowl_temp").innerText = "Текущая температура: " + parseInt(e.features[0].properties.boiler_t);
+    document.getElementById("address_b").innerText = e.features[0].properties.boiler_addr;
+    document.getElementById("house_temp").innerText = "Текущая температура: " + parseInt(e.features[0].properties.t);
+    document.getElementById("address_h").innerText = e.features[0].properties.address;
+
+});
+
+map.on('click','boiler', function (e) {
+    document.getElementById("info_hot_bowl").style.visibility = "visible";
+    document.getElementById("hot_bowl_temp").innerText = "Текущая температура: " + parseInt(e.features[0].properties.t);
+    document.getElementById("address_b").innerText = e.features[0].properties.address;
+
+});
+
+map.on('mouseenter', 'client', function () {
+    map.getCanvas().style.cursor = 'pointer';
+});
+
+map.on('mouseenter', 'boiler', function () {
+    map.getCanvas().style.cursor = 'pointer';
+});
+
+map.on('mouseleave', 'client', function () {
+    map.getCanvas().style.cursor = '';
+});
+
+map.on('mouseleave', 'boiler', function () {
+    map.getCanvas().style.cursor = '';
 });
 
 map.on('load', function () {
 
+    var boiler_pos;
+
+        map.loadImage('https://img.icons8.com/ios/50/000000/factory-filled.png', function(error, image) {
+            if (error) throw error;
+            map.addImage('factory', image);
+        });
+
+        map.loadImage("https://img.icons8.com/ios/50/000000/cottage-filled.png", function (error, image) {
+            if (error) throw error;
+            map.addImage('house', image);
+        });
+
+    $.ajax({
+        url: linking + "boiler",
+        type: 'get',
+        success(data){
+            boiler_pos = data.features[0].geometry.coordinates;
+            console.log(boiler_pos);
+            map.addSource("boiler-source", {
+                "type" : "geojson",
+                "data" : data
+            });
+            map.addLayer({
+                id: 'boiler',
+                type: 'symbol',
+                source: 'boiler-source',
+                "layout":{
+                    "icon-image": "factory",
+                    "icon-size" : 1
+                },
+                filter: ['in', '$type', 'Point']
+            })
+        }
+    });
+
+    console.log(boiler_pos);
+
+    $.ajax({
+        url: linking + "client",
+        type: 'get',
+        success(data){
+            map.addSource("client-source", {
+                "type" : "geojson",
+                "data" : data
+            });
+            map.addLayer({
+                id: 'client',
+                type: 'symbol',
+                layout:{
+                    "icon-image":"house",
+                    "icon-size":0.5
+                },
+                source: 'client-source',
+                filter: ['in', '$type', 'Point']
+            });
+
+            var data_special = {
+                type: "FeatureCollection",
+                features: []
+            };
+
+            var data_special2 = {
+                type: "FeatureCollection",
+                features: []
+            };
+
+            for (let i=0;i<data.features.length;i++){
+                if (i>0) {
+                    data_special.features.push({
+                        geometry: {
+                            type: "LineString",
+                            coordinates: [data.features[i].geometry.coordinates, [37.525434494018555, 55.57664649599778]]
+                        }
+                    });
+                }
+                else{
+                    data_special2.features.push({
+                        geometry: {
+                            type: "LineString",
+                            coordinates: [data.features[i].geometry.coordinates, [37.525434494018555, 55.57664649599778]]
+                        }
+                    });
+                }
+            };
+
+
+
+            console.log(data_special);
+
+            map.addSource("lines-source", {
+                "type" : "geojson",
+                "data" : data_special
+            });
+            map.addLayer({
+                id: 'lines',
+                type: 'line',
+                source: 'lines-source',
+                layout: {
+                    'line-cap': 'round',
+                    'line-join': 'round'
+                },
+                paint: {
+                    'line-color': '#00ff00',
+                    'line-width': 4.0
+                },
+                filter: ['in', '$type', 'LineString']
+            });
+
+            map.addSource("lines-source2", {
+                "type" : "geojson",
+                "data" : data_special2
+            });
+            map.addLayer({
+                id: 'lines2',
+                type: 'line',
+                source: 'lines-source2',
+                layout: {
+                    'line-cap': 'round',
+                    'line-join': 'round'
+                },
+                paint: {
+                    'line-color': '#ff0000',
+                    'line-width': 4.0
+                },
+                filter: ['in', '$type', 'LineString']
+            })
+
+        }
+    });
+
 });
+
+function relocation() {
+    localStorage.setItem("adr", document.getElementById("address_h").innerText);
+    location.replace("info_page.html")
+}
